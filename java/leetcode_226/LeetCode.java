3ff6b8ad6fa74a47ded81e3bc2c4458f9ee86d06
ryanfakir <ryanfakir@gmail.com>
2016-09-15 17:58:39 -0400

    add java 226

---
+++ java/leetcode_226/LeetCode.java
@@ -0,0 +1,25 @@
+package leetcode_226;
+/**
+ * Created by pyan on 9/15/2016.
+ */
+class LeetCode {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    public TreeNode invertTree(TreeNode root) {
+        //corner case
+        if (root == null) {
+            return root;
+        }
+        //basic logic
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
